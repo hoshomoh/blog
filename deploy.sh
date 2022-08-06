42e983f0eede25579d446647ab09d176d0895fc6
Oshomo Oforomeh <oshomo.oforomeh2010@gmail.com>
2022-08-06 18:03:54 +0200

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,19 @@
 # Create a temporary directory and store its name in a variable.
 TEMP_DIR=$(mktemp -d)
 NOW=$(date +"%D %T")
-COMMIT_MSG="Site updated at $nNOW"
+COMMIT_MSG="Site updated at $NOW"
 
 # Exit if the temp directory wasn't created successfully.
-if [ ! -e "TEMP_DIR" ]; then
+if [ ! -e "$TEMP_DIR" ]; then
     >&2 echo "Failed to create temp directory"
     exit 1
 fi
 
 # Move generated site to temporary directory
-mv _site/* "${TEMP_DIR}"
+mv _site/* $TEMP_DIR
 git checkout -B master
 rm -rf *
-mv "${TEMP_DIR}/*" .
+mv "$TEMP_DIR/*" .
 
 # Push changes to github
 git add .
